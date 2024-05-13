c4b847b806bd023a9fb94018127da8e8113ea7d8
Aryan <aryanc0809@gmail.com>
2024-05-13 14:15:20 +1000

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,10 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Testing
+          Hello :0
         </p>
         <a
           className="App-link"
